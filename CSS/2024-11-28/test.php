c9b8be0a5762e17ef010ee890fed39a243ce26ac
onion <a0966550619@gmail.com>
2024-11-28 15:22:28 +0800

    完成js作業,click,addevent練習

---
+++ CSS/2024-11-28/test.php
@@ -0,0 +1,15 @@
+<?php
+    $name = 'onion';
+    echo("Hi $name");
+
+    //array
+
+    //index array
+    $indexArrindexArr = [1,2,3];
+    //Associative array
+    $assocArr = [
+        's1' => 'amy',
+        's2' => 'bob',
+        's3' => 'cat'
+    ];
+?>
